1be3b929ca7932c914abc8edcd04626ed84f7b9f
nlanners <nicholas.lanners@gmail.com>
2025-11-07 21:14:18 -0700

    Implement enemy movement and better spawn logic Adjust positions

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -10,23 +10,41 @@ signal enemy_hit
 @onready var enemy_2: AnimatedSprite2D = $Enemy2
 @onready var enemy_3: AnimatedSprite2D = $Enemy3
 
+var move_timer = 0
+var move_interval = 0.5
+
+var animation_node: AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if not enemy_type:
-		enemy_type = randi_range(1, 3) as EnemyType
+		enemy_type = [EnemyType.ONE, EnemyType.TWO, EnemyType.THREE].pick_random()
 	match enemy_type:
 		EnemyType.ONE:
 			enemy_1.visible = true
+			animation_node = enemy_1
 		EnemyType.TWO:
 			enemy_2.visible = true
+			animation_node = enemy_2
 		EnemyType.THREE:
 			enemy_3.visible = true
+			animation_node = enemy_3
+	
+	add_to_group("enemies")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	move_timer += delta
+	if move_timer >= move_interval:
+		move_timer = 0
+
+		if animation_node.frame == 0:
+			animation_node.frame = 1
+		else:
+			animation_node.frame = 0
+			
+			
 
 
 func hit():
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,33 +2,63 @@ extends Node2D
 
 const NUMBER_OF_ROWS: int = 3
 const ROW_WIDTH: int = 10
-const FIELD_LEFT: int = 300
-const FIELD_TOP: int = 275 + 16
+const FIELD_LEFT: int = 0
+const FIELD_TOP: int = 16
 const ENEMY_HEIGHT: int = 32
 const ENEMY_WIDTH: int = 32
 
 enum EnemyType { NONE, ONE, TWO, THREE }
 
-var score = 0
+var score: int = 0
+
+const MOVE_SPEED = 20
+
+var direction: int = 1
+var move_timer: float = 0
+var move_interval: float = 0.5
+var should_drop: bool = false
 
 const Enemy = preload("res://scenes/enemy.tscn")
 
+@onready var enemy_box: Area2D = $EnemyBox
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for i in NUMBER_OF_ROWS * ROW_WIDTH:
 		var enemy = Enemy.instantiate()
-		enemy.add_to_group("enemies")
 		enemy.enemy_hit.connect(_on_enemy_hit)
-		enemy.enemy_type = [EnemyType.ONE, EnemyType.TWO, EnemyType.THREE].pick_random()
 		@warning_ignore("integer_division")
-		enemy.global_position = Vector2(
-			FIELD_LEFT + (ENEMY_WIDTH / 2) + ((i % ROW_WIDTH) * ENEMY_WIDTH),
-			FIELD_TOP + ((i / ROW_WIDTH) * ENEMY_HEIGHT)
+		enemy.position = Vector2(
+			(ENEMY_WIDTH / 2) + ((i % ROW_WIDTH) * ENEMY_WIDTH),
+			(ENEMY_HEIGHT / 2) + ((i / ROW_WIDTH) * ENEMY_HEIGHT)
 		)
-		add_child(enemy)
+		enemy_box.add_child(enemy)
+
+
+func _process(delta) -> void:
+	process_enemy_movement(delta)
+	
+	if get_tree().get_node_count_in_group("enemies") <= 0:
+		pass
 
 
 func _on_enemy_hit():
 	score += 1
-	print(score)
+
+
+func _on_enemy_box_body_entered(_body: Node2D) -> void:
+	direction *= -1
+	should_drop = true
+	
+
+func process_enemy_movement(delta) -> void:
+	move_timer += delta
+
+	if move_timer >= move_interval:
+		move_timer = 0
+		
+		if should_drop:
+			enemy_box.position.y += ENEMY_HEIGHT
+			should_drop = false
+		else:
+			enemy_box.position.x += MOVE_SPEED * direction
